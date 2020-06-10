eec10d0e447653e0cd529980c18858c62d729ab3
Namin <snamin@mcw.edu>
2020-06-09 21:34:00 -0500

    Data explorer

---
+++ ZIPCode_Explorer/server.R
@@ -0,0 +1,96 @@
+library(leaflet)
+library(RColorBrewer)
+library(scales)
+library(lattice)
+library(dplyr)
+
+allzips <- readRDS("C:/Users/snamin/Documents/GitHub/Developing-Data-Products-course-Week-4-Shiny-Application-and-Reproducible-Pitch/data/superzip.rds")
+allzips$latitude <- jitter(allzips$latitude)
+allzips$longitude <- jitter(allzips$longitude)
+allzips$college <- allzips$college * 100
+allzips$zipcode <- formatC(allzips$zipcode, width=5, format="d", flag="0")
+row.names(allzips) <- allzips$zipcode
+
+cleantable <- allzips %>%
+  select(
+    City = city.x,
+    State = state.x,
+    Zipcode = zipcode,
+    Rank = rank,
+    Score = centile,
+    Superzip = superzip,
+    Population = adultpop,
+    College = college,
+    Income = income,
+    Lat = latitude,
+    Long = longitude
+  )
+
+# Leaflet bindings are a bit slow; for now we'll just sample to compensate
+set.seed(100)
+zipdata <- allzips[sample.int(nrow(allzips), 10000),]
+# By ordering by centile, we ensure that the (comparatively rare) SuperZIPs
+# will be drawn last and thus be easier to see
+zipdata <- zipdata[order(zipdata$centile),]
+
+function(input, output, session) {
+  
+  
+  ## Data Explorer ###########################################
+  
+  observe({
+    cities <- if (is.null(input$states)) character(0) else {
+      filter(cleantable, State %in% input$states) %>%
+        `$`('City') %>%
+        unique() %>%
+        sort()
+    }
+    stillSelected <- isolate(input$cities[input$cities %in% cities])
+    updateSelectizeInput(session, "cities", choices = cities,
+                         selected = stillSelected, server = TRUE)
+  })
+  
+  observe({
+    zipcodes <- if (is.null(input$states)) character(0) else {
+      cleantable %>%
+        filter(State %in% input$states,
+               is.null(input$cities) | City %in% input$cities) %>%
+        `$`('Zipcode') %>%
+        unique() %>%
+        sort()
+    }
+    stillSelected <- isolate(input$zipcodes[input$zipcodes %in% zipcodes])
+    updateSelectizeInput(session, "zipcodes", choices = zipcodes,
+                         selected = stillSelected, server = TRUE)
+  })
+  
+  observe({
+    if (is.null(input$goto))
+      return()
+    isolate({
+      map <- leafletProxy("map")
+      map %>% clearPopups()
+      dist <- 0.5
+      zip <- input$goto$zip
+      lat <- input$goto$lat
+      lng <- input$goto$lng
+      showZipcodePopup(zip, lat, lng)
+      map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
+    })
+  })
+  
+  output$ziptable <- DT::renderDataTable({
+    df <- cleantable %>%
+      filter(
+        Score >= input$minScore,
+        Score <= input$maxScore,
+        is.null(input$states) | State %in% input$states,
+        is.null(input$cities) | City %in% input$cities,
+        is.null(input$zipcodes) | Zipcode %in% input$zipcodes
+      ) %>%
+      mutate(Action = paste('<a class="go-map" href="" data-lat="', Lat, '" data-long="', Long, '" data-zip="', Zipcode, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
+    action <- DT::dataTableAjax(session, df, outputId = "ziptable")
+    
+    DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
+  })
+}
---
+++ ZIPCode_Explorer/ui.R
@@ -0,0 +1,69 @@
+library(leaflet)
+library(dplyr)
+
+allzips <- readRDS("C:/Users/snamin/Documents/GitHub/Developing-Data-Products-course-Week-4-Shiny-Application-and-Reproducible-Pitch/data/superzip.rds")
+allzips$latitude <- jitter(allzips$latitude)
+allzips$longitude <- jitter(allzips$longitude)
+allzips$college <- allzips$college * 100
+allzips$zipcode <- formatC(allzips$zipcode, width=5, format="d", flag="0")
+row.names(allzips) <- allzips$zipcode
+
+cleantable <- allzips %>%
+  select(
+    City = city.x,
+    State = state.x,
+    Zipcode = zipcode,
+    Rank = rank,
+    Score = centile,
+    Superzip = superzip,
+    Population = adultpop,
+    College = college,
+    Income = income,
+    Lat = latitude,
+    Long = longitude
+  )
+
+
+# Choices for drop-downs
+vars <- c(
+  "Is SuperZIP?" = "superzip",
+  "Centile score" = "centile",
+  "College education" = "college",
+  "Median income" = "income",
+  "Population" = "adultpop"
+)
+
+
+navbarPage("ZIPCode Explorer", id="nav",
+           
+           
+           tabPanel("Data explorer",
+                    fluidRow(
+                      column(3,
+                             selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
+                      ),
+                      column(3,
+                             conditionalPanel("input.states",
+                                              selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+                             )
+                      ),
+                      column(3,
+                             conditionalPanel("input.states",
+                                              selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
+                             )
+                      )
+                    ),
+                    fluidRow(
+                      column(1,
+                             numericInput("minScore", "Min score", min=0, max=100, value=0)
+                      ),
+                      column(1,
+                             numericInput("maxScore", "Max score", min=0, max=100, value=100)
+                      )
+                    ),
+                    hr(),
+                    DT::dataTableOutput("ziptable")
+           ),
+           
+           conditionalPanel("false", icon("crosshair"))
+)
